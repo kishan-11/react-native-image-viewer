b30195ea6c1c7fd48a6ea200100b04217e148cfc
Kishan Patel <kishankumar.patel@proptiger.com>
2019-05-31 11:42:04 +0530

    initial commit

--- src/image-viewer.component.tsx
+++ src/image-viewer.component.tsx
@@ -641,16 +641,17 @@ export default class ImageViewer extends React.Component<Props, State> {
    * 保存当前图片到本地相册
    */
   public saveToLocal = () => {
-    if(this.props.imageUrls[this.state.currentShowIndex || 0].src) {
-      if (!this.props.onSave) {
+    if (!this.props.onSave) {
+      if(this.props.imageUrls[this.state.currentShowIndex || 0].src) {
         CameraRoll.saveToCameraRoll(this.props.imageUrls[this.state.currentShowIndex || 0].src);
-        this!.props!.onSaveToCamera!(this.state.currentShowIndex);
-      } else {
+      }
+      this!.props!.onSaveToCamera!(this.state.currentShowIndex);
+    } else {
+      if(this.props.imageUrls[this.state.currentShowIndex || 0].src) {
         this.props.onSave(this.props.imageUrls[this.state.currentShowIndex || 0].src);
       }
-  
-      this.setState({ isShowMenu: false });
-    };
+    }
+    this.setState({ isShowMenu: false });
   };
 
   public getMenu() {
